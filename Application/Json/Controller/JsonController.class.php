efb7c7ad4407b5e62c68fa93f0d815ba66b66f8f
Kepler <314027027@qq.com>
2018-04-09 14:59:38 +0800

    添加查看信息接口

--- Application/Json/Controller/JsonController.class.php
+++ Application/Json/Controller/JsonController.class.php
@@ -614,4 +614,15 @@ class JsonController extends Controller {
         //
     }
 
+    //查看用户信息
+    public function load_user_info(){
+        $uid = I('request.uid');
+        $user = M('user');
+        $data = $user -> where("uid = '$uid'") -> field('ualiase,ulevel,utype') -> find();
+        $attention = M('attention');
+        $data['attention_user'] = $attention -> where("id = '$uid' and state = 11") ->count();
+        $data['user_attention'] = $attention -> where(" auid = '$uid' and state = 11") -> count();
+        $this->ajaxReturn($data);
+    }
+
 }
